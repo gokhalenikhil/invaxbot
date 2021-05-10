397a6561b2ab8d3c957a434b8b7510f945d82def
Deepak Mawane <deepmwn@gmail.com>
2021-05-10 11:25:35 +0530

    main: Added the notification service project. The notification service will send alerts to the subscribed user whenever centers are availabe near him/her.

---
+++ notificationservice/Invaxbot.NotificationService.NotificationLambda/Models/CuratedCenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Invaxbot.NotificationService.NotificationLambda.Models
+{
+    public class CuratedCenter 
+    {
+        public long center_id { get; set; }
+        public string name { get; set; }
+        public string pincode { get; set; }
+        public bool eighteenplusAvailability { get; set; }
+        public bool fortyfiveplusAvailability { get; set; }
+        public double weeklyCapacity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}, {pincode}, {weeklyCapacity} slots available\n";
+        }
+    }
+}
---
+++ notificationservice/Invaxbot.NotificationService.NotificationLambda/Models/NotificationLambdaInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Invaxbot.NotificationService.NotificationLambda.Models
+{
+    public class NotificationLambdaInput
+    {
+        public string short_pin { get; set; }
+
+        public string s3url { get; set; }
+
+        public string bucket { get; set; }
+    }
+}
